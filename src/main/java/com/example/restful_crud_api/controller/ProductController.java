22b115d9d0845de619fe3a64fb35b0a9f6c90477
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2024-12-29 17:25:51 -0300

    Add PUT
    Add PUT endpoint to update product

--- src/main/java/com/example/restful_crud_api/controller/ProductController.java
+++ src/main/java/com/example/restful_crud_api/controller/ProductController.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.swing.plaf.PanelUI;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -50,4 +50,18 @@ public class ProductController {
         productO.get().add(linkTo(methodOn(ProductController.class).getAllProducts()).withRel("Products list"));
         return ResponseEntity.status(HttpStatus.OK).body(productO.get());
     }
+    @PutMapping("products/{id}")
+    public ResponseEntity<Object>updateProducts(@PathVariable(value = "id") UUID id,
+                                                @RequestBody @Valid ProductRecordDto productRecordDto){
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if (productO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Products not found");
+        }
+        var productModel = productO.get();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
+    }
+
+  
+
 }
