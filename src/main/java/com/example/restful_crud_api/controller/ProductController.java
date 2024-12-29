6802479b2bd25bf77df41c5d0b0e1e537ef90bf8
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2024-12-29 16:49:12 -0300

    Implement product retrieval by ID
    Implement product retrieval by ID in API

--- src/main/java/com/example/restful_crud_api/controller/ProductController.java
+++ src/main/java/com/example/restful_crud_api/controller/ProductController.java
@@ -8,11 +8,15 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.swing.plaf.PanelUI;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class ProductController {
@@ -25,5 +29,25 @@ public class ProductController {
         BeanUtils.copyProperties(productRecordDto, productModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
     }
-    
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts(){
+        List<ProductModel> productlList = productRepository.findAll();
+        if (!productlList.isEmpty()){
+        for (ProductModel product : productlList){
+            UUID id = product.getIdProduct();
+            product.add(linkTo(methodOn(ProductController.class).getOneProduct(id)).withSelfRel());
+         }
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(productlList);
+    }
+    @GetMapping("products/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id")UUID id){
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if (productO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        productO.get().add(linkTo(methodOn(ProductController.class).getAllProducts()).withRel("Products list"));
+        return ResponseEntity.status(HttpStatus.OK).body(productO.get());
+    }
 }
