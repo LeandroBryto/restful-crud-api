272f1c7d2117f8aaaa97133dd7e77608be9056a0
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2024-12-29 16:10:00 -0300

    Add POST method
    Add POST method to create resourc

--- src/main/java/com/example/restful_crud_api/controller/ProductController.java
+++ src/main/java/com/example/restful_crud_api/controller/ProductController.java
@@ -1,4 +1,29 @@
 package com.example.restful_crud_api.controller;
 
+import com.example.restful_crud_api.dtos.ProductRecordDto;
+import com.example.restful_crud_api.model.ProductModel;
+import com.example.restful_crud_api.repository.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ProductController {
+    @Autowired
+    ProductRepository productRepository;
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel>saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+    
 }
--- src/main/java/com/example/restful_crud_api/repository/ProductRepository.java
+++ src/main/java/com/example/restful_crud_api/repository/ProductRepository.java
@@ -1,8 +1,11 @@
 package com.example.restful_crud_api.repository;
 
+import com.example.restful_crud_api.model.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface ProductRepository extends JpaRepository {
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
 }
